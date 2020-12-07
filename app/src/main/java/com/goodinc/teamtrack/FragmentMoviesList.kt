8b1dee505b38d94612cefe54cad55f005714a544
Andrey Sakhno <andrey.sakhno@1win.pro>
2020-12-07 21:37:36 +0300

    add click listener, add backstack navigation, handle device rotation

---
+++ app/src/main/java/com/goodinc/teamtrack/FragmentMoviesList.kt
@@ -0,0 +1,34 @@
+package com.goodinc.teamtrack
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesList : Fragment() {
+    private var cardClickListener: ClickMovieCardListener? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
+            View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<View>(R.id.movie_card)?.setOnClickListener{
+            cardClickListener?.onClickCard()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is ClickMovieCardListener) {
+            cardClickListener = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        cardClickListener = null
+    }
+}
--- app/src/main/java/com/goodinc/teamtrack/MoviesActivity.kt
+++ app/src/main/java/com/goodinc/teamtrack/MoviesActivity.kt
@@ -2,15 +2,24 @@ package com.goodinc.teamtrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
-class MoviesActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity(), ClickMovieCardListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.main_container, FragmentMoviesList())
+                    .commit()
+        }
+    }
 
+    override fun onClickCard() {
         supportFragmentManager.beginTransaction()
                 .add(R.id.main_container, FragmentMoviesDetails())
+                .addToBackStack(null)
                 .commit()
-
     }
 }
