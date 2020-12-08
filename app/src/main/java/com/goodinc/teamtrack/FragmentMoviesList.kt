b64dd789d6b30b392ea0e6c084b4b21665fc5355
Andrey Sakhno <andrey.sakhno@1win.pro>
2020-12-08 15:34:05 +0300

    add missed changes on FragmentMoviesList

--- app/src/main/java/com/goodinc/teamtrack/FragmentMoviesList.kt
+++ app/src/main/java/com/goodinc/teamtrack/FragmentMoviesList.kt
@@ -7,32 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class FragmentMoviesList : Fragment() {
+class FragmentMoviesList : Fragment(R.layout.fragment_movies_list) {
     companion object{
         fun newInstance() : FragmentMoviesList = FragmentMoviesList()
     }
 
-    private var cardClickListener: ClickMovieCardListener? = null
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
-            View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
+    private val cardClickListener: ClickMovieCardListener? get() = (activity as? ClickMovieCardListener)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<View>(R.id.movie_card)?.setOnClickListener{
+        view.findViewById<View>(R.id.movie_card)?.setOnClickListener {
             cardClickListener?.onClickCard()
         }
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is ClickMovieCardListener) {
-            cardClickListener = context
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        cardClickListener = null
     }
 }
